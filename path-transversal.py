085cd1ea750bf57a8e1f3279caa373ea13d22b7a
IkariTyux <git@rph.red>
2024-11-14 11:48:47 +0100

    The script is working

--- path-transversal.py
+++ path-transversal.py
@@ -1,4 +1,26 @@
-import os
+import urllib
+import requests
 
-print("Trying to get the webserver file : ")
-os.system('curl --path-as-is http://localhost:8080/../server.py')
+files = ["../server.py", "../../../../../../etc/passwd", "../../../../.bash_history"]
+
+
+def try_GET():
+    url = f'http://localhost:8080/{file}'
+    request = urllib.request.urlopen(url)
+    code = request.status
+
+    match code:
+        case 200:
+            print(f"{file} is exposed to the public.")
+        case 404:
+            print(f"{file} is secure.")
+        case 403:
+            print(f"Access forbidden. But {file} is exposed")
+        case 503:
+            print("Service Unavailable")
+        case _:
+            print(f"{code} : Unknown error code")
+
+
+for file in files:
+    try_GET()
